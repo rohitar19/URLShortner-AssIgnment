dcc7b026868b91b6f69823b5327c3255bf73abc5
rohitar19 <rohit960211@gmail.com>
2024-02-07 05:27:50 +0530

    extra

--- index.js
+++ index.js
@@ -26,6 +26,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+
+
 app.use("/url", restrictToLoggedinUserOnly, urlRoute);
 app.use("/user", userRoute);
 app.use("/", checkAuth, staticRoute);
