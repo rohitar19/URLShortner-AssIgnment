126475c17b93fb9dcc53785f1d133f9c81d52da8
rohitar19 <rohit960211@gmail.com>
2024-03-18 03:08:44 +0530

    deployed to ec2

--- index.js
+++ index.js
@@ -11,11 +11,16 @@ const staticRoute = require("./routes/staticRouter");
 const userRoute = require("./routes/user");
 
 const app = express();
-const PORT = 8001;
+const PORT = 4000;
 
-connectToMongoDB(process.env.MONGODB ?? "mongodb+srv://rohit960211:rohit@cluster0.qfbl9nq.mongodb.net/URLShortner?retryWrites=true&w=majority").then(() =>
+const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
+
+connectToMongoDB(process.env.MONGODB ?? `mongodb+srv://rohit960211:${password}@cluster0.qfbl9nq.mongodb.net/URLShortner?retryWrites=true&w=majority`).then(() =>
   console.log("Mongodb connected")
 );
+
+app.use("Access-Control-Allows-Origin" , "*");
+app.use("Access-Control-Allows-Header" , "*");
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
